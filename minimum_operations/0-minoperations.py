95d0ae0734d8c463cfd72b944832508274206807
MaryNayram <m.doh@alustudent.com>
2023-09-12 01:05:35 -0700

    0-minoperations.py

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Minimum Operations"""
+
+
+def minOperations(n):
+        """Calculates the number of operations to result n H characters"""
+            min_operation = 0
+                factor = 2
+
+                    while n > 1:
+                                if n % factor == 0:
+                                                min_operation += factor
+                                                            n //= factor
+                                else:
+                                                factor += 1
+
+                                                    return min_operation
+                                                
